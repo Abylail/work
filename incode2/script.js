0a66767b99941c4e68b5f29f75abd5c5f98d8730
Abylail <47676619+Abylail@users.noreply.github.com>
2020-04-02 23:00:50 +0600

    Add files via upload

---
+++ incode2/script.js
@@ -0,0 +1,35 @@
+var header = document.querySelector('#header');
+var text1 = document.querySelector('#text-box-1');
+var check = document.querySelector('#check');
+
+//opacity after px
+
+document.addEventListener('scroll', function() {
+  	if (window.pageYOffset > 50) {
+    	header.classList.add('op');
+
+    	text1.classList.add('op2');
+    	text1.classList.remove('op1');
+
+  	} else {
+    	header.classList.remove('op');
+
+    	text1.classList.remove('op2');
+    	text1.classList.add('op1');
+
+  	}
+});
+
+//hide in scrolling
+var srollN = 0;
+
+window.addEventListener('scroll', function() {
+	check.innerHTML = pageYOffset;
+	if(pageYOffset>30 && pageYOffset > scrollN){
+		header.classList.add('out');
+	}
+	else{
+		header.classList.remove('out');
+	}
+	scrollN = pageYOffset;
+});
